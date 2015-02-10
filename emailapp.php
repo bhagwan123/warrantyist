a84480f2beaa755eb0137b1d8511a43e54ede2b0
bhagwan123 <bhagwanpawar07@gmail.com>
2015-02-10 19:51:54 +0530

    master file

---
+++ emailapp.php
@@ -0,0 +1,10 @@
+<?php
+$to      = 'vaibhavshivdas@yahoo';
+$subject = 'the subject';
+$message = 'hello';
+$headers = 'From: noreply@designbuildapp.in' . "\r\n" .
+    'Reply-To: noreply@designbuildapp.in' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+?>
